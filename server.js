dd416279a062f029ecb51b7ff78d1b3b699e61e2
Serdar <udemuradovv@gmail.com>
2021-10-11 11:25:38 +0500

    add

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/', (req, res) =>{
         service: 'gmail',
         auth: {
             user: 'balkanturkmentranzit@gmail.com',
-            pass: 'bntt26.08.21'
+            pass: ''
         }
     })
     const mailOptions = {
